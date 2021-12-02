da6523897b051989d057287fcc2f734838b28d93
tmdan <mukhammed.ne@gmail.com>
2021-12-02 23:01:15 +0300

    added

--- index.php
+++ index.php
@@ -1,9 +1,40 @@
-<?php
-?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Emelyanov</title>
+</head>
+<body>
 
-<h1>Welcome</h1>
-
+<h1>Личная страница Емельянова Никиты!</h1>
 
 <ul>
-    <li><a href="./ivanov/index.php">Иванов</a></li>
+
+    <li>Работа с HTML тегами
+        <ul>
+            <li><a href="lesson-2tmlTeg.php">HW HTML</a></li>
+            <li><a href="lesson-2/boolean_values.php">Логические значения в PHP</a></li>
+        </ul>
+    </li>
+    <li>3 занятие
+        <ul>
+            <li><a href="NullPHPull.php">Null в php</a></li>
+            <li><a href="arrHomeWorkndex.php">HM arr</a></li>
+        </ul>
+    </li>
+    <li>4 занятие
+        <ul>
+            <li><a href="cyclesHomeWorkndex.php">cyclesHMWork</a></li>
+            <li><a href="IfElseHWndex.php">If else HW</a></li>
+        </ul>
+    </li>
+
+
 </ul>
+
+
+</body>
+</html>
