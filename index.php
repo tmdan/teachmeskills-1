ae3e9345e93062f4d62ce226a4b11fb753e7b0d0
Roman Piletski <blender-04@yandex.com>
2021-12-02 23:41:04 +0300

    Made all tickets

--- index.php
+++ index.php
@@ -13,28 +13,28 @@
     <ul>
         <li class="lessons">2 занятие - Типы данных, операции
             <ul>
-                <li class="lessons_task"><a href="lesson-2ull.php">Значение null в PHP</a></li>
-                <li class="lessons_task"><a href="lesson-2oolean_values.php">Логические значения в PHP</a></li>
-                <li class="lessons_task"><a href="lesson-2tring_values.php">Строки в PHP</a></li>
-                <li class="lessons_task"><a href="lesson-2nteger_values.php">Числа и операции над ними в PHP</a></li>
-                <li class="lessons_task"><a href="lesson-2ractice_work.php">Практика по работе с переменными и операциями в PHP</a></li>
-                <li class="lessons_task"><a href="lesson-2ractice_tag.php">Работа с HTML тегами в PHP</a></li>
+                <li class="lessons_task"><a href="lesson-2/null.php">Значение null в PHP</a></li>
+                <li class="lessons_task"><a href="lesson-2/boolean_values.php">Логические значения в PHP</a></li>
+                <li class="lessons_task"><a href="lesson-2/string_values.php">Строки в PHP</a></li>
+                <li class="lessons_task"><a href="lesson-2/integer_values.php">Числа и операции над ними в PHP</a></li>
+                <li class="lessons_task"><a href="lesson-2/practice_work.php">Практика по работе с переменными и операциями в PHP</a></li>
+                <li class="lessons_task"><a href="lesson-2/practice_tag.php">Работа с HTML тегами в PHP</a></li>
             </ul>
         </li>
         <li class="lessons">3 занятие - Условная операции if..else
             <ul>
-                <li class="lessons_task"><a href="lesson-3icket134_green.php">Задачи по if...else - Задача по часам и порам года</a></li>
-                <li class="lessons_task"><a href="lesson-3icket124_yellow.php">Задачи по if...else - Задача про функцию</a></li>
-                <li class="lessons_task"><a href="lesson-3icket113_red.php">Задачи по if...else - Трудная задача про функцию</a></li>
+                <li class="lessons_task"><a href="lesson-3/ticket134_green.php">Задачи по if...else - Задача по часам и порам года</a></li>
+                <li class="lessons_task"><a href="lesson-3/ticket124_yellow.php">Задачи по if...else - Задача про функцию</a></li>
+                <li class="lessons_task"><a href="lesson-3/ticket113_red.php">Задачи по if...else - Трудная задача про функцию</a></li>
             </ul>
         </li>
         <li class="lessons">4 занятие - Массивы и циклы
             <ul>
-                <li class="lessons_task"><a href="lesson-4icket188_green.php">Задача по массивам - Ключи, значения </a></li>
-                <li class="lessons_task"><a href="lesson-4icket147_red.php">Задача по массивам - Перестановка элементов в массиве </a></li>
-                <li class="lessons_task"><a href="lesson-4icket179_green.php">Задача по циклам - Массив из нечетных чисел через цикл и функцию </a></li>
-                <li class="lessons_task"><a href="lesson-4icket160_yellow.php">Задача по циклам - Расчет факториала числа </a></li>
-                <li class="lessons_task"><a href="lesson-4icket170_red.php">Задача по циклам - Вложенные циклы для сложных строк </a></li>
+                <li class="lessons_task"><a href="lesson-4/ticket188_green.php">Задача по массивам - Ключи, значения </a></li>
+                <li class="lessons_task"><a href="lesson-4/ticket147_red.php">Задача по массивам - Перестановка элементов в массиве </a></li>
+                <li class="lessons_task"><a href="lesson-4/ticket179_green.php">Задача по циклам - Массив из нечетных чисел через цикл и функцию </a></li>
+                <li class="lessons_task"><a href="lesson-4/ticket160_yellow.php">Задача по циклам - Расчет факториала числа </a></li>
+                <li class="lessons_task"><a href="lesson-4/ticket170_red.php">Задача по циклам - Вложенные циклы для сложных строк </a></li>
             </ul>
         </li>
         <li class="lessons">5 занятие -
