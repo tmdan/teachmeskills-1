28038c54eab54afb9598e73802896cc42b350af1
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2021-12-03 00:45:02 +0300

    Add files via upload
    add a index for odd numbers

--- PHP_Homework_1/php/Сycles/cycles task(easy).php
+++ PHP_Homework_1/php/Сycles/cycles task(easy).php
@@ -3,16 +3,17 @@
 //С помощью цикла заполните массив нечетными числами в промежутке от 1 до 99.
 function insertInArr($first,$last){
     $arr = [];
-    for($first;$first<=$last;$first++):
+    for($first;$first<=$last;$first+=2):
         array_push($arr,$first);
     endfor;
     return $arr;
 }
 var_dump(insertInArr(1,99));
+echo "<br>";
 
 //С помощью готовой функции range заполните массив нечетными числами в промежутке от 1 до 99.
 function insertOddNum($first,$last){
-        $arr = range($first,$last,2);
-    return $arr;
+        $arr_1 = range($first,$last,2);
+    return $arr_1;
 }
 print_r(insertOddNum(1,99));
