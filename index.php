b97c294587052135a2407ea131764dfa8eb513a8
tmdan <mukhammed.ne@gmail.com>
2021-12-02 22:49:41 +0300

    added

--- index.php
+++ index.php
@@ -1,9 +1,40 @@
-<?php
-?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 
-<h1>Welcome</h1>
-
+<h1>Личная страница Ольги Доженок</h1>
 
 <ul>
-    <li><a href="./ivanov/index.php">Иванов</a></li>
+
+    <li>Занятие 2. Типы данных, операции.
+        <ul>
+            <li><a href="lesson2/null.php">Null в php</a></li>
+            <li><a href="lesson2/logic_values.php">Логические значения в php</a></li>
+            <li><a href="lesson2/strings.php">Строки в php</a></li>
+            <li><a href="lesson2/numbers.php">Числа и операции над ними в php</a></li>
+            <li><a href="lesson2/variables.php">Работе с переменными</a></li>
+            <li><a href="lesson2tml_tags.php">Работа с HTML тегами</a></li>
+        </ul>
+    </li>
+
+
+    <li>Занятие 3.
+        <ul>
+            <li><a href="#">....</a></li>
+            <li><a href="#">...</a></li>
+        </ul>
+    </li>
+
+
 </ul>
+
+
+</body>
+</html>
