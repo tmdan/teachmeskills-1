1179cbd09a2dbbb0d6340ff1587974168beb3df4
tmdan <mukhammed.ne@gmail.com>
2021-12-02 23:10:11 +0300

    added

--- index.php
+++ index.php
@@ -1,9 +1,49 @@
-<?php
-?>
-
-<h1>Welcome</h1>
-
-
-<ul>
-    <li><a href="./ivanov/index.php">Иванов</a></li>
-</ul>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Roman Piletski</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Личная страница Романа Пилецкого</h1>
+    <ul>
+        <li class="lessons">2 занятие - Типы данных, операции
+            <ul>
+                <li class="lessons_task"><a href="lesson-2ull.php">Значение null в PHP</a></li>
+                <li class="lessons_task"><a href="lesson-2oolean_values.php">Логические значения в PHP</a></li>
+                <li class="lessons_task"><a href="lesson-2tring_values.php">Строки в PHP</a></li>
+                <li class="lessons_task"><a href="lesson-2nteger_values.php">Числа и операции над ними в PHP</a></li>
+                <li class="lessons_task"><a href="lesson-2ractice_work.php">Практика по работе с переменными и операциями в PHP</a></li>
+                <li class="lessons_task"><a href="lesson-2ractice_tag.php">Работа с HTML тегами в PHP</a></li>
+            </ul>
+        </li>
+        <li class="lessons">3 занятие - Условная операции if..else
+            <ul>
+                <li class="lessons_task"><a href="lesson-3icket134_green.php">Задачи по if...else - Задача по часам и порам года</a></li>
+                <li class="lessons_task"><a href="lesson-3icket124_yellow.php">Задачи по if...else - Задача про функцию</a></li>
+                <li class="lessons_task"><a href="lesson-3icket113_red.php">Задачи по if...else - Трудная задача про функцию</a></li>
+            </ul>
+        </li>
+        <li class="lessons">4 занятие - Массивы и циклы
+            <ul>
+                <li class="lessons_task"><a href="lesson-4icket188_green.php">Задача по массивам - Ключи, значения </a></li>
+                <li class="lessons_task"><a href="lesson-4icket147_red.php">Задача по массивам - Перестановка элементов в массиве </a></li>
+                <li class="lessons_task"><a href="lesson-4icket179_green.php">Задача по циклам - Массив из нечетных чисел через цикл и функцию </a></li>
+                <li class="lessons_task"><a href="lesson-4icket160_yellow.php">Задача по циклам - Расчет факториала числа </a></li>
+                <li class="lessons_task"><a href="lesson-4icket170_red.php">Задача по циклам - Вложенные циклы для сложных строк </a></li>
+            </ul>
+        </li>
+        <li class="lessons">5 занятие -
+            <ul>
+                <li class="lessons_task"><a href="#">...</a></li>
+                <li class="lessons_task"><a href="#">...</a></li>
+                <li class="lessons_task"><a href="#">...</a></li>
+            </ul>
+        </li>
+    </ul>
+</body>
+</html>
