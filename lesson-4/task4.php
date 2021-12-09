851861aa3e0181211f8e8edd3f452f9aee59d592
DmitriiSavchik <dmitrii.savchik@gmail.com>
2021-12-09 20:16:22 +0300

    task 2 array_red new

--- lesson-4/task4.php
+++ lesson-4/task4.php
@@ -1,6 +1,10 @@
 <?php
 $arr=[1,2,3,4,5];
-$newArr = array_reverse($arr);
-
-echo '<pre>' . var_export($arr) . '</pre>';
-echo '<pre>' . var_export($newArr) . '</pre>';
+$numArr = count ($arr)-1;
+$newArr=[];
+for($i=$numArr; $i>=0; $i--){
+    $newArr=$arr[$i];
+    var_dump($newArr);
+}
+//echo '<pre>' . var_export($arr) . '</pre>';
+//echo '<pre>' . var_export($newArr) . '</pre>';
