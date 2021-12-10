8d4989dff6e66a5d59c093cf06da4d597f3fe9e5
IlyaPrushak <prushak.ilya.sergeevich@gmail.com>
2021-12-10 19:05:42 +0300

    "[#230] recursion_average"

--- recursion/recursion_average.php
+++ recursion/recursion_average.php
@@ -3,20 +3,28 @@
 //Пример: я в функцию передаю число 179. На экран после выполнения нашей рекурсивной функции должно выводиться 1+7+9 = 17
 //При решении этой задачи нельзя использовать массивы ну и циклы, разумеется
 
-$a = explode(".",149);
-$b = strlen(149)-1;
-$num=$a[0][$b];
-$arr=[];
-array_push($arr,$a[0][$b-1]);
-array_push($arr,$a[0][$b-2]);
-array_push($arr,$a[0][$b-3]);
+//$a = explode(".",149);
+//$b = strlen(149)-1;
+//$num=$a[0][$b];
+//$arr=[];
+//array_push($arr,$a[0][$b-1]);
+//array_push($arr,$a[0][$b-2]);
+//array_push($arr,$a[0][$b-3]);
 
 
-function sum($arr,$index){
-    if($index>=0){
-        return $arr[$index] + sum($arr,$index-1);
-        echo $arr[$index];
-    }else return 0;
+//function sum($arr,$index){
+//    if($index>=0){
+//        return $arr[$index] + sum($arr,$index-1);
+//        echo $arr[$index];
+//    }else return 0;
+//}
+//echo(sum($arr,2));
+
+function sum($num){
+    if($num==0){
+        return 0;
+    }
+    else return $num%10+sum((int)($num/10));
 }
-echo(sum($arr,2));
+echo(sum(1494));
 
