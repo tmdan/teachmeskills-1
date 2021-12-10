a3c0cf15d417f172d476af2bb7bb72130149ccf1
MaxSl77 <maximslabko77@icloud.com>
2021-12-10 12:29:03 +0300

    lesson3 easy level

--- slabko/arrays/lesson3(easy).php
+++ slabko/arrays/lesson3(easy).php
@@ -6,5 +6,21 @@ $flipped = array_flip($arr);
 echo array_sum($flipped);
 echo "<br>";
 echo $a = array_sum($arr)/array_sum($flipped);
-echo $a = array_sum($arr)/array_sum($flipped);
+echo "<br>";
+?>
+
+<!--lesson 3 easy, second part-->
+<?php
+$arr1 = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5];
+$arr2 = [];
+$arr3 = [];
+foreach ($arr1 as $key => $value) {
+    array_push($arr2, $key);
+    array_push($arr3, $value);
+}
+
+var_dump($arr2);
+echo "<br>";
+var_dump($arr3);
+
 ?>
