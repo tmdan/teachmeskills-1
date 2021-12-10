0c206c9c05f21aa15d4bef950b2f9587a930378c
MaxSl77 <maximslabko77@icloud.com>
2021-12-10 21:33:41 +0300

    lesson4 easy level

---
+++ slabko/cycle/cycle easy leve.php
@@ -0,0 +1,17 @@
+<?php
+$array = [];
+for ($i = 0; $i <= 99; $i++) {
+if($i % 2 != 0) {
+    echo $i , ' , ' ;
+}}
+echo '<br>';
+?>
+
+<!--second lesson cycle-->
+<?php
+$array1 = range(0,100);
+for ($a = 0; $a <= 99; $a++) {
+    if($a % 2 != 0) {
+        echo $a , ' , ' ;
+    }}
+?>
