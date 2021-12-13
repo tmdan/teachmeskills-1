6a5fe1746eb0ca60540cb6a509c8c66dcc7e3efc
MaxSl77 <maximslabko77@icloud.com>
2021-12-13 23:25:27 +0300

    commit

--- index.php
+++ index.php
@@ -2,34 +2,48 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
+<div class="head_information">
+<h1>Личная страница Максима Слабко</h1>
+</div>
 
-<h1>Личная страница Иванова</h1>
-
+<div class="body_information">
 <ul>
 
-    <li>2 занятие - Типы данных, операции
+    <li>2 занятие
         <ul>
-            <li><a href="lesson-2/null.php">Значение null в PHP</a></li>
-            <li><a href="lesson-2/boolean_values.php">Логические значения в PHP</a></li>
+            <li><a href="./slabko/personalInfo.php">Личный данные</a></li>
+            <li><a href="./slabko/boolean.php">Логические значения в PHP</a></li>
         </ul>
     </li>
 
 
     <li>3 занятие
         <ul>
-            <li><a href="#">....</a></li>
-            <li><a href="#">...</a></li>
+            <li><a href="./lesson-with-if_else">ДЗ по If...Else</a></li>
+        </ul>
+    </li>
+
+    <li>4 занятие
+        <ul>
+            <li><a href="./slabko/arrays/lesson3(easy).php">ДЗ по Массивам, Легкий уровень</a></li>
+            <li><a href="./slabko/arrays/lesson3(hard).php">ДЗ по Массивам, Сложный уровень</a></li>
+            <li><a href="./slabko/cycle/cycle%20easy%20leve.php">ДЗ по Циклам, Легкий уровень</a></li>
+            <li><a href="./slabko/cycle/cycle%20medium%20level.php">ДЗ по Циклам, Средний уровень</a></li>
+            <li><a href="./slabko/cycle/cycle%20hard%20level.php">ДЗ по Циклам, Сложный уровень</a></li>
+            <li><a href="./slabko/recursia/recursia%20medium.php">ДЗ по Рекурсии, Средний уровень</a></li>
+            <li><a href="./slabko/recursia/recursia%20hard.php">ДЗ по Массивам, Сложный уровень</a></li>
         </ul>
     </li>
 
 
 </ul>
+</div>
 
 
 </body>
