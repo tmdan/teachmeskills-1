ec383715565b2e630d459c1db1b419b8f7de881f
nick_019@mail.ru <nick_019@mail.ru>
2022-01-09 01:11:24 +0300

    change reg expression

--- Homeworks/lesson_9/search_HTML_tags.php
+++ Homeworks/lesson_9/search_HTML_tags.php
@@ -1,14 +1,12 @@
 <?php
 echo '<h3>'.'Написать регулярное выражение для поиска html тегов <strong>какой то текст</strong> внутри поисковой строки:.</h3>';
-echo htmlspecialchars('тут возможен текст <strong>какой то текст</strong> и тут текст <input>какой то текст</input> и тут снова текст<div>какой то текст</div><label>какой то текст</label>');
+echo htmlspecialchars('тут возможен текст <strong>какой то текст</strong> и тут текст <div>какой то текст</div> и тут снова текст<div>какой то текст</div><label>какой то текст</label>');
 echo '<br>';
 
-$pattern = '~<[a-z]+>.+?<[^>]+>~';
-$str= 'тут возможен текст <strong>какой то текст</strong> и тут текст <strong>какой то текст</strong> и тут снова текст<strong>какой то текст</strong><strong>какой то текст</strong>';
+$pattern = '/<.+?>.+?<\/.+?>/';
+$str= 'тут возможен текст <strong>какой то текст</strong> и тут текст <div>какой то текст</div> и тут снова текст<div>какой то текст</div><label>какой то текст</label>';
 $result = [];
 
 preg_match_all($pattern, $str, $result) ;
-echo '<pre>';
-print_r($result[0]);
-echo '</pre>';
+
 echo "найдено ".count($result[0])." совпадений: <br>";
