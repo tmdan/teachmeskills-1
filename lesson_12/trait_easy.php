78b860fda7c6429a93f84fbd9d0746a44bddd9b8
unknown <panymari84@gmail.com>
2022-01-19 00:55:12 +0300

    #384-Задача по трейтам (просто)

---
+++ lesson_12/trait_easy.php
@@ -0,0 +1,35 @@
+<?php
+trait Trait1{
+    public function method1(){
+        return 1;
+    }
+}
+
+trait Trait2{
+    public function method2(){
+        return 2;
+    }
+}
+
+trait Trait3{
+    public function method3(){
+        return 3;
+    }
+}
+
+interface TraitInterface{
+    public function method1();
+    public function method2();
+    public function method3();
+    public function getSum();
+}
+
+class Test implements TraitInterface{
+    use Trait1, Trait2, Trait3;
+    public function getSum(){
+        echo "Sum = ".$this->method1()+$this->method2()+$this->method3();
+    }
+}
+
+$test = new Test();
+$test->getSum();
