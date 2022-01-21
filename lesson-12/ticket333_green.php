66358a9a3e43c6b19351ee8d9c8a24b012398d4a
Roman Piletski <blender-04@yandex.com>
2022-01-21 16:01:28 +0300

    #333 - Магические методы(легко)

--- lesson-12/ticket333_green.php
+++ lesson-12/ticket333_green.php
@@ -2,4 +2,25 @@
     echo "создайте любой объект<br><br>
 внедрите в ваш класс 4 любых магических метода<br><br>
 покажите реальные примеры использование данных магических методов<br><br>";
+    class travelAgency {
+        private $name;
+        private $numberWorkers;
+        public function __construct($name) {
+            $this->name = $name;
+        }
+        public function __set($name, $value) {
+            echo "Устанавливаем число сотрудников в фирме $this->name равным $value <br><br>";
+            $this->numberWorkers = $value;
+        }
+        public function __get($numberWorkers) {
+            return $this->numberWorkers;
+        }
+        public function __toString() {
+            return $this->name;
+        }
+    }
+$travelCompany = new travelAgency("Roman-Travel");
+$travelCompany->numberWorkers = 15;
+echo "Выводим число сотрудников $travelCompany->numberWorkers<br><br>";
+echo $travelCompany;
 
