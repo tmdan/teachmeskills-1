d5d490f7f7283e18d61910d68f11a33de105384c
IlyaPrushak <prushak.ilya.sergeevich@gmail.com>
2022-01-23 19:42:16 +0300

    "[#346] Задача #7 - ООП (интерфейсы) (очень сложно)"

--- interface/Задача #7 - ООП (интерфейсы) (очень сложно).php
+++ interface/Задача #7 - ООП (интерфейсы) (очень сложно).php
@@ -1,102 +1,155 @@
 <?php
-interface CarInterface{
-    public function carOn();
-    public function carOff();
+interface TransportInterface{
+    public function TransportOn();
+    public function TransportOff();
 }
 interface EngineInterface{
     public function engineOn();
     public function engineOff();
 }
-class Car implements CarInterface{
+class ShowRoom{
+    private array $shop=[];
     private string $name;
-    private string $color;
-    private bool $toggle=false;
-    private bool $goIn = false;
-    private bool $isBus = false;
-    private $engine;
-    public function __construct($name,$color,$engine)
+    public function __construct($name)
     {
         $this->name=$name;
-        $this->color=$color;
-        $this->engine=$engine;
-        $this->setBus();
     }
-    public function carOn(){$this->toggle = true;}
-    public function carOff(){$this->toggle = false;}
-    public function getCarOn(){return $this->toggle;}
-    public function getCarOff(){return $this->toggle;}
-    public function getEngineOn(){return $this->engine->getEngineOn();}
-    public function getEngineOff(){return $this->engine->getEngineOff();}
-    private function check(){
-        if($this->name==="Bus"){
-            return true;
-        }else return false;
+    public function addTransport(TransportInterface $transport){
+        array_push($this->shop,$transport);
     }
-    private function setBus(){if($this->check()) $this->isBus=true;}
-    private function getBus(){return $this->isBus;}
-    public function letPeopleGoIn(){if($this->getBus()) $this->goIn=true;}
-    public function dontLetPeopleGoIn(){if(!$this->getBus()) $this->goIn=false;}
-    public function showBus(){
-        if($this->goIn){
-            echo "There is a BUS. People can go in to go Minsk <br>";
-        }
-    }
-}
-class Engine implements EngineInterface{
-    private string $brand;
-    private bool $toggle=false;
-    public function __construct($brand,$power){
-        $this->brand=$brand;
-        $this->power=$power;
-    }
-    public function engineOn(){$this->toggle = true;}
-    public function engineOff(){$this->toggle = false;}
-    public function getEngineOn(){return $this->toggle;}
-    public function getEngineOff(){return $this->toggle;}
-}
-class AutoRoom{
-    private string $name;
-    private array $places=[];
-    private $car;
-    public function __construct($name){$this->name=$name;}
-    public function addCar($car){
-        if($this->checkCar($car))
-            array_push($this->places,$car);
-    }
-    private function checkCar($car){
-        if($car->getCarOn() and $car->getEngineOn()){
-            return true;
-        }else return false;
-    }
-    public function showCars(){
-        foreach ($this->places as $car){
-            var_dump($car);
+    public function show(){
+        foreach ($this->shop as $index){
+            var_dump($index);
             echo "<br>";
         }
     }
 }
-$engine = new Engine("Logen",200);
-$engine->engineOn();
-$car = new Car("Truck","red",$engine);
-$car->carOn();
+abstract class Transport implements TransportInterface {
+    protected string $brand;
+    protected $engine;
+    public function __construct($brand,EngineInterface $engine)
+    {
+        $this->brand=$brand;
+        $this->engine=$engine;
+    }
+    abstract public function action();
+}
+abstract class Engine implements EngineInterface{
+    protected string $name;
+    protected int $power;
+    public function __construct($name,$power)
+    {
+        $this->name=$name;
+        $this->power=$power;
+    }
+    abstract public function showStatment();
+}
+class Bus extends Transport {
+    private int $amountOfPassangers;
+    public function __construct($brand, EngineInterface $engine,$amountOfPassangers)
+    {
+        parent::__construct($brand, $engine);
+        $this->amountOfPassangers=$amountOfPassangers;
+    }
+    public function TransportOn()
+    {
+        $this->engine->engineOn();
+    }
+    public function TransportOff()
+    {
+        $this->engine->engineOff();
+    }
+    public function action(){
+        $this->amountOfPassangers>0?$this->takePassanger():$this->takeOffPassanger();
+    }
+    private function takePassanger(){
+       echo "bus take some passengers <br>";
+    }
+    private function takeOffPassanger(){
+        echo "bus take off some passengers <br>";
+    }
+}
+class BusEngine extends Engine{
+    public function __construct($name, $power)
+    {
+        parent::__construct($name, $power);
+    }
+    public function show()
+    {
+        //something....
+    }
+    public function engineOn()
+    {
+        echo "engine on by key";
+    }
+    public function engineOff()
+    {
+        echo "engine off by key";
+    }
+    public function showStatment()
+    {
+        //some statment....
+    }
+}
+class TruckEngine extends Engine{
+    public function __construct($name, $power)
+    {
+        parent::__construct($name, $power);
+    }
+    public function show()
+    {
+        //something....
+    }
+    public function engineOn()
+    {
+        echo "engine on by touch";
+    }
+    public function engineOff()
+    {
+        echo "engine off by touch";
+    }
+    public function showStatment()
+    {
+        //some statment....
+    }
+}
+class Truck extends Transport {
+    private int $tonns;
+    public function __construct($brand, EngineInterface $engine,$tonns)
+    {
+        parent::__construct($brand, $engine);
+        $this->tonns=$tonns;
+    }
+    public function TransportOn()
+    {
+        $this->engine->engineOn();
+    }
+    public function TransportOff()
+    {
+        $this->engine->engineOff();
+    }
+    public function action(){
+        $this->tonns>0?$this->takeTonns():$this->takeOffTonns();
+    }
+    private function TakeTonns(){ //загрузить
+        echo "truck take some tonns <br>";
+    }
+    private function takeOffTonns(){ //выгрузить
+        echo "truck take off some tonns <br>";
+    }
+}
+
+$showroom=new ShowRoom("Shop");
+
+$bus = new Bus("BMW",new BusEngine("Pro-100",1000),10);
+$bus->action();
+$showroom->addTransport($bus);
+
+$truck = new Truck("Belarus",new BusEngine("Bel24",2000),100);
+$truck->action();
+$showroom->addTransport($truck);
 
 
-$engine1 = new Engine("Inshe2",300);
-$engine1->engineOn();
-$car1 = new Car("Car","pink",$engine);
-$car1->carOn();
+$showroom->show();
 
-$engine2 = new Engine("Inshe",100);
-$engine2->engineOn();
-$car2 = new Car("Bus","blue",$engine);
-$car2->carOn();
 
-$autoRoom = new AutoRoom("IlyaHouse");
-
-$autoRoom->addCar($car);
-$autoRoom->addCar($car1);
-$autoRoom->addCar($car2);
-$autoRoom->showCars();;
-
-$car2->LetPeopleGoIn();
-$car2->showBus();
