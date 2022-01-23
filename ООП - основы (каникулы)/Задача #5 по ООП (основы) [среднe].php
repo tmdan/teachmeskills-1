42d6559281f355d72e487aea1d7876fd7cc8c2b7
IlyaPrushak <prushak.ilya.sergeevich@gmail.com>
2022-01-24 00:57:06 +0300

    "[#362] Задача #5 по ООП (основы) [среднe]"

--- ООП - основы (каникулы)/Задача #5 по ООП (основы) [среднe].php
+++ ООП - основы (каникулы)/Задача #5 по ООП (основы) [среднe].php
@@ -23,127 +23,118 @@
 //- salery - доход от 1 просмотра в долларах (генериться рандомно от 1 до 3 долларов)
 //
 //Публичные методы:
-//- Получить количество просмотров - для каждого видео генериться рандомно от 1 до 10000 просмотров прямо в сеттере установите рандом
+//- Получить количество просмотров - для каждого видео генериться рандомно от 1 до 10000
+// просмотров прямо в сеттере установите рандом
 //- Установить имя видео
-class Channel{
-    private string $name;
-    private $video;
+class Channel
+{
+    private string $channel;
     private array $videos=[];
-    private int $generalViews=0;
-    private int $generalIncome=0;
-    private array $mostPopularVideo=[];
-    private array $mostIncomeVideo=[];
-    public function __construct($name)
+    private int $generalAmountOfViews=0;
+    private int $generalAmountOfIncome=0;
+    private array $incomeVideo=[]; // массив для хранения дохода за каждое видео
+    private array $viewsVideo=[];  // массив для хранения количество просмотро за каждое видео
+    private array $theMostPopularVideo=[]; //массив для хранения 1-го самого популярного видео
+    private array $theMostIncomeVideo=[];  //массив для хранения 1-го самого доходного видео
+    public function __construct($channel)
     {
-        $this->name=$name;
+        $this->channel=$channel;
+    }
+    public function getChannel(){
+        return $this->channel;
     }
     public function addVideo($video){
-        $this->video=$video;
         array_push($this->videos,$video);
-        array_push($this->mostPopularVideo,$video->getAmountOfViews());
-        array_push($this->mostIncomeVideo,$video->getAmountOfViews()*$video->getIncomeForOneVideo());
+        $this->incomeVideo[]=$video->getIncomeForViews();
+        $this->viewsVideo[]=$video->getViews();
     }
-    public function generalViews(){
-        foreach ($this->videos as $index)
-            $this->generalViews+=$index->getAmountOfViews();
-    }
-    public function getGeneralViews(){
-        return $this->generalViews;
-    }
-    public function generalIncome(){
-        foreach ($this->videos as $index)
-            $this->generalIncome+=$index->getIncomeForOneVideo()*$index->getAmountOfViews();
-    }
-    public function getGeneralIncome(){
-        return $this->generalIncome;
-    }
-    public function show(){
-        echo "channel: ".$this->name."<br>";
-        foreach ($this->videos as $index){
-            var_dump($index);
-            echo "<br>";
+    public function showListOfVidios(){
+        foreach ($this->videos as $video){
+            var_dump($video); echo "<br>";
         }
-        echo "general views: ".$this->getGeneralViews()."<br>";
-        echo "general income: ".$this->getGeneralIncome()."<br>";
-        echo "the most popular video: ";
-        var_dump($this->theMostPopularVideo());
-        echo "<br> the most income video: ";
-        var_dump($this->theMostIncomeVideo());
-
     }
-    public function theMostPopularVideo(){
-        for($a=0;$a<count($this->videos)-1;$a++){
-
-            for($i=0;$i<count($this->mostIncomeVideo)-1;$i++){
-                $elem1=$this->mostPopularVideo[$i];
-                $elem2=$this->mostPopularVideo[$i+1];
-                if($elem1<$elem2){
-                    $this->mostPopularVideo[$i]=$elem2;
-                    $this->mostPopularVideo[$i+1]=$elem1;
-                }
+    private function setGeneralAmountOfViews(){ // общее количество просмотров
+        foreach ($this->videos as $video){
+            $this->generalAmountOfViews+=$video->getViews();
+        }
+    }
+    public function getGeneralAmountOfViews(){
+        $this->setGeneralAmountOfViews(); //вызываю здесь так как более удачного места не вижу
+        return $this->generalAmountOfViews;
+    }
+    private function setGeneralAmountOfIncome(){
+        foreach ($this->videos as $video){
+            $this->generalAmountOfIncome+=$video->getIncomeForViews();
+        }
+    }
+    public function getGeneralAmountOfIncome(){
+        $this->setGeneralAmountOfIncome(); //вызываю здесь так как более удачного места не вижу
+        return  $this->generalAmountOfIncome;
+    }
+    private function sortVideos(){ //сортировка
+        sort($this->viewsVideo);
+        sort($this->incomeVideo);
+    }
+    private function setTheMostVideo(){ //установка самого популярного и доходного видео
+        $this->sortVideos();
+        for ($i=0;$i<count($this->videos);$i++){
+            if($this->videos[$i]->getIncomeForViews() === $this->incomeVideo[count($this->incomeVideo)-1]){
+                $this->theMostIncomeVideo[]=$this->videos[$i];
             }
-        }
-        return $this->checkTheMostPopularVideo();
-    }
-    private function checkTheMostPopularVideo(){
-        for($a=0;$a<count($this->videos);$a++){
-            if($this->videos[$a]->getAmountOfViews()===$this->mostPopularVideo[0]){
-                return $this->videos[$a];
+            if($this->videos[$i]->getViews() === $this->viewsVideo[count($this->viewsVideo)-1]){
+                $this->theMostPopularVideo[]=$this->videos[$i];
             }
         }
     }
-    public function theMostIncomeVideo(){
-        for($a=0;$a<count($this->videos)-1;$a++){
-
-            for($i=0;$i<count($this->mostIncomeVideo)-1;$i++){
-                $elem1=$this->mostIncomeVideo[$i];
-                $elem2=$this->mostIncomeVideo[$i+1];
-                if($elem1<$elem2){
-                    $this->mostIncomeVideo[$i]=$elem2;
-                    $this->mostIncomeVideo[$i+1]=$elem1;
-                }
-            }
-        }
-        return $this->checkTheMostIncomeVideo();
-    }
-    private function checkTheMostIncomeVideo(){
-        for($a=0;$a<count($this->videos);$a++){
-            if($this->videos[$a]->getIncomeForOneVideo()*$this->videos[$a]->getAmountOfViews()===$this->mostIncomeVideo[0]){
-                return $this->videos[$a];
-            }
-        }
+    public function showTheMostVideo(){
+        $this->setTheMostVideo();
+        echo "The most Popular video: ";
+        var_dump($this->theMostPopularVideo);
+        echo "<br>";
+        echo "The most Income video: ";
+        var_dump($this->theMostIncomeVideo);
+        echo "<br>";
     }
 
 }
 class Video{
     private string $name;
-    private int $amountOfViews;
-    private int $incomeForOne;
+    private int $views;
+    private int $incomeForViews;
     public function __construct($name)
     {
         $this->name=$name;
-        $this->amountOfViews=rand(1,10000);
-        $this->incomeForOne=rand(1,3);
+        $this->setViews();
+        $this->setIncomeForViews();
     }
-    public function show(){
-        echo "video: ".$this->name." ,amount of income for a video: ".$this->getIncomeForOneVideo()."$<br>";
+    private function setViews(){
+        $this->views=rand(1,10000);
     }
-    public function getAmountOfViews(){
-        return $this->amountOfViews;
+    private function setIncomeForViews(){
+        $this->incomeForViews = rand(1,3)*$this->views;
     }
-    public function getIncomeForOneVideo(){
-        return $this->incomeForOne;
+    public function getViews(){
+        return $this->views;
+    }
+    public function getIncomeForViews(){
+        return $this->incomeForViews;
+    }
+    public function getName(){
+        return $this->name;
     }
 }
-$video_1 = new Video("My little pony");
-$video_2 = new Video("Me and you");
-$channel = new Channel("Prushak");
+$channel = new Channel("TheKing");
+echo "Channel: ".$channel->getChannel()."<br>";// лень было создавать метод show (думаю не ошибка)
+echo "Channel have videos: ".$channel->getChannel()."<br>";// лень было создавать метод show (думаю не ошибка)
+$video_1 = new Video("I'm a KING");
+$video_2 = new Video("My little pony");
+$video_3 = new Video("Ilya know PhP");
 $channel->addVideo($video_1);
 $channel->addVideo($video_2);
-$video_1->show();
-$video_2->show();
-$channel->generalViews();
-$channel->generalIncome();
-$channel->theMostIncomeVideo();
-$channel->theMostPopularVideo();
-$channel->show();
+$channel->addVideo($video_3);
+$channel->showListOfVidios();
+echo "General views: ".$channel->getGeneralAmountOfViews(); // лень было создавать метод show (думаю не ошибка)
+echo "<br>";
+echo "General income: ".$channel->getGeneralAmountOfIncome();// лень было создавать метод show (думаю не ошибка)
+echo "<br>";
+$channel->showTheMostVideo();
