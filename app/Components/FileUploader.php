acadfab97774551e04b0b75c3b632fa219e8240a
Maria Pashkovskaya <panymari84@gmail.com>
2022-03-11 12:56:11 +0300

    #497-Задача по MVC - Компонент работы с загрузкой файлов.

---
+++ app/Components/FileUploader.php
@@ -0,0 +1,12 @@
+<?php
+class FileUploader{
+    public static function to($url){
+        $uploads_dir = '/uploads';
+        $tmp_name = $url["tmp_name"];
+        $name = basename($url["name"]);
+        move_uploaded_file($tmp_name, "$uploads_dir/$name");
+    }
+    public static function remove($url){
+        unlink($url);
+    }
+}
