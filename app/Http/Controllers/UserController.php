98bb5eea3939b21809e24cd8b83dc069f79fc244
Roman Piletski <blender-04@yandex.com>
2022-03-25 19:21:50 +0300

    Laravel tasks 515

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index($name)
+    {
+        echo "City - " . $name;
+    }
+
+    public function show($name)
+    {
+        $users = [
+            'Roma' => 'Minsk',
+            'Lesha' => 'Moskva',
+            'Ivan' => 'Madrid',
+            'Rita' => 'Rome',
+            'Sasha' => 'Pinsk',
+        ];
+        return view("user.show", ["city"=> $users[$name]]);
+    }
 }
