6f3d1ac8155bee002ecab6b1b65b28753d4332a1
EkaterinaKhliabets <e.v.khliabets@gmail.com>
2022-03-31 01:26:45 +0300

    #535 - задача #2 - добавление связей на уровне моделей

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function сomments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
