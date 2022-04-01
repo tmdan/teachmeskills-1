c10601b3120bcca0f12ea17bcd48c8bdcdef068d
EkaterinaKhliabets <e.v.khliabets@gmail.com>
2022-04-01 17:49:17 +0300

    'changed the name of the migration'

--- database/migrations/2022_04_01_143031_create_post_tag_table.php
+++ database/migrations/2022_04_01_143031_create_post_tag_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('post_tags', function (Blueprint $table) {
+        Schema::create('post_tag' , function (Blueprint $table) {
             $table->id();
             $table->foreignId('post_id')->constrained('posts')->cascadeOnDelete();
             $table->foreignId('tag_id')->constrained('tags')->cascadeOnDelete();
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('post_tags');
+        Schema::dropIfExists('post_tag');
     }
 };
