d22973fb9adbd188c28fb08cedab6fe0d7a23694
nick_019@mail.ru <nick_019@mail.ru>
2022-04-02 11:38:53 +0300

    add methods in model Comments

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -18,4 +18,35 @@ class Comment extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function publish()
+    {
+        $this->is_published = 1;
+        $this->save();
+    }
+
+    public function unpublish()
+    {
+        $this->is_published = 0;
+        $this->save();
+    }
+
+    public function togglePublish($value)
+    {
+        if ($this->is_published == 0)
+        {
+            return $this->publish();
+        }
+        return $this->unpublish();
+    }
+
+    public function scopePublished($query)
+    {
+        return $query->where('is_published', 1);
+    }
+
+    public function scopeUnpublished($query)
+    {
+        return $query->where('is_published', 0);
+    }
 }
