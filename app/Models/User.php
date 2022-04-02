bd60bb012a5ab357b6766850b76830ffa619fc63
nick_019@mail.ru <nick_019@mail.ru>
2022-04-02 12:43:51 +0300

    add attribute to model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -50,5 +52,12 @@ class User extends Authenticatable
     public function comments()
     {
         return $this->hasMany(Comment::class);
+    }
+
+    public function passwordHash():Attribute
+    {
+        return Attribute::make(
+            set: fn($value) => Hash::make($value)
+        );
     }
 }
