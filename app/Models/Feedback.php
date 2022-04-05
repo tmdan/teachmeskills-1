e20081fa585f4a60a361c777306f27eea5e3d009
Roman Piletski <blender-04@yandex.com>
2022-04-05 16:25:35 +0300

    #550 #551 made migrations, models

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -2,10 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Feedback extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function scopePublished($query)
+    {
+        return $query->where("is_publish", 1);
+    }
+
+    protected function body(): Attribute
+    {
+        return Attribute::make(
+            get: function ($value) {
+                $user = User::find($this->id)->first();
+                return $value . " from " . $user->name;
+            }
+        );
+    }
 }
