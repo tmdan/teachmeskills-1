bb1dafc2ba9ee922cb78fc927839f1c19496764c
nick_019@mail.ru <nick_019@mail.ru>
2022-04-26 13:31:58 +0300

    fix validation rules in StoreUserRequest

--- app/Http/Requests/Admin/StoreUserRequest.php
+++ app/Http/Requests/Admin/StoreUserRequest.php
@@ -25,7 +25,7 @@ class StoreUserRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:255',
-            'email' => 'required|string',
+            'email' => 'required|email',
             'avatar' => 'image|max:2048',
             'password' => 'required|string'
         ];
--- app/Observers/Admin/CategoryObserver.php
+++ app/Observers/Admin/CategoryObserver.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Observers;
+namespace App\Observers\Admin;
 
 use App\Models\Category;
 
