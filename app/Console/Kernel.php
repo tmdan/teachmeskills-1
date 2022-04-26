079348fd7e5c943427de4adc8dcbb644939afd8d
EkaterinaKhliabets <e.v.khliabets@gmail.com>
2022-04-27 01:11:29 +0300

    daily()

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,8 +15,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //$schedule->command('users:force-delete')->daily();
-        $schedule->command('users:force-delete')->everyMinute();
+        $schedule->command('users:force-delete')->daily();
+        //$schedule->command('users:force-delete')->everyMinute();
     }
 
     /**
