73cc99416763ce6d2c4ca28db8252f8055171306
EkaterinaKhliabets <e.v.khliabets@gmail.com>
2022-05-03 23:45:28 +0300

    #659 - Задача #13 - Клиентская часть (настройка статики) - Webpack Mix

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-
-   echo '123';
-
-});
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
 
 Route::group(['prefix'=>'admin'], function(){
     Route::get('/', [\App\Http\Controllers\Admin\DashboardController::class, 'index']);
