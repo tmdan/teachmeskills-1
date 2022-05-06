4164c7ab169bfa23f33466cfdfb222e48f06defe
Maria Pashkovskaya <panymari84@gmail.com>
2022-05-06 13:12:17 +0300

    #603-UserObserver

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use App\Notifications\SingUpNotification;
+use Illuminate\Support\Facades\Storage;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "updated" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function updated(User $user)
+    {
+        //
+    }
+
+    public function deleting(User $user){
+
+        if ($user->avatar != User::NO_IMAGE && Storage::exists($user->avatar)) {
+            Storage::delete($user->avatar);
+        }
+
+    }
+    /**
+     * Handle the User "deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function deleted(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function restored(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function forceDeleted(User $user)
+    {
+        //
+    }
+}
