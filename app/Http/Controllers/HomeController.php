0e7aa6043bac60e661ad0f5b1cbf55a5d269e0a6
tmdan <mukhammed.ne@gmail.com>
2022-05-09 22:02:58 +0300

    App Service Providers / Service Controllers and Outer Api using Example

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,19 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
 use App\Services\Weather\Template\WeatherServiceInterface;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
+
 
 class HomeController extends Controller
 {
     public function index(WeatherServiceInterface $openWeatherApiService)
     {
-
-
-        dd($openWeatherApiService->coordinates()->getCityName());
-
+        //dd($openWeatherApiService->coordinates()->getCityName());
 
 //        $data = Http::get("https://api.openweathermap.org/data/2.5/weather?q=Minsk&appid=5cc014b5ee90117eb3ad4f8c758455c4&lang=ru&units=metric")->json();
 //
@@ -23,6 +18,6 @@ class HomeController extends Controller
 
         //dd((new OpenWeatherApiService())->getInfo()->main);
 
-       // return view('pages.index', ['posts' => Post::with(['author', 'category'])->paginate(1)]);
+       return view('pages.index', ['posts' => Post::with(['author', 'category'])->paginate(1)]);
     }
 }
