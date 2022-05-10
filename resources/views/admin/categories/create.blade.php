89eef1a832a56fe8f5a3e3c5f88717f5a1326a1b
MaxSl77 <maximslabko77@icloud.com>
2022-05-10 14:21:58 +0300

    laravelBlogpicture

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -17,7 +17,7 @@
             <!-- Default box -->
             <div class="box">
                 <form method="POST" action="{{route("admin.categories.store")}}">
-                    @@csrf
+                    @csrf
                     @method("POST")
                     <div class="box-header with-border">
                         <h3 class="box-title">Добавляем категорию</h3>
