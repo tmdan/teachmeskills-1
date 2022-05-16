6bf64f197554dc5dbf0c1e247435eed3a3148a0f
MaxSl77 <maximslabko77@icloud.com>
2022-05-16 15:08:55 +0300

    laravelBlogTaskManager

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
 {
     public function index(IndexUserRequest $request)
     {
-        $users = Users::all();
+        $users = User::all();
 
         return view('admin.users.index', ['users' => $users]);
     }
