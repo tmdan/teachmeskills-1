5ac17c9ac75493b5e1bf2bfdc57580de7acea969
EkaterinaKhliabets <e.v.khliabets@gmail.com>
2022-05-23 18:28:06 +0300

    исправена ссылка на верификацию

--- resources/views/emails/verification.blade.php
+++ resources/views/emails/verification.blade.php
@@ -1,3 +1,3 @@
 <h1>Спасибо, кликните по ссылке</h1>
-<a href="http://php-laravel/verification/{{$subscriber->token}}">{{$subscriber->token}}</a>
-{{$subscriber->email}}
+<!--<a href="http://php-laravel/verification/{{$subscriber->token}}">{{$subscriber->token}}</a> -->
+<a href="{{url('/')}}/verification/{{$subscriber->token}}">{{$subscriber->token}}</a>
